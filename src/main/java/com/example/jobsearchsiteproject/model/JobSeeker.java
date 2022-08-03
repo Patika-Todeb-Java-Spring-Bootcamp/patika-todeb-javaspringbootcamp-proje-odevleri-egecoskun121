54163d4bb2b815cf374170ac3248d4dab1a97971
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-08-03 17:48:44 +0300

    Mapping

--- src/main/java/com/example/jobsearchsiteproject/model/JobSeeker.java
+++ src/main/java/com/example/jobsearchsiteproject/model/JobSeeker.java
@@ -5,14 +5,25 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.Set;
+
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="jobseekers")
 public class JobSeeker extends Person{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long jobSeekerId;
     private double currentSalary;
     private boolean isSalaryMonthly;
     private String currency;
-    private ExperienceDetail experienceDetail;
-    private EducationDetail educationDetail;
+    @OneToMany(mappedBy = "",cascade = CascadeType.MERGE,fetch = FetchType.LAZY)
+    private Set<ExperienceDetail> experienceDetailSet;
+    @OneToMany()
+    private Set<EducationDetail> educationDetailSet;
 
 }
