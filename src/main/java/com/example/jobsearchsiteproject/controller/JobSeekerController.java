e29c72e2e75474efaece14c3c6798deaeaf41867
egecoskun121 <77891949+egecoskun121@users.noreply.github.com>
2022-08-03 13:37:05 +0300

    Added controller

---
+++ src/main/java/com/example/jobsearchsiteproject/controller/JobSeekerController.java
@@ -0,0 +1,54 @@
+package com.example.jobsearchsiteproject.controller;
+
+
+import com.example.jobsearchsiteproject.dto.JobSeekerDTO;
+import com.example.jobsearchsiteproject.model.JobSeeker;
+import com.example.jobsearchsiteproject.services.JobSeekerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/jobseeker")
+public class JobSeekerController {
+
+    @Autowired
+    private JobSeekerService jobSeekerService;
+
+    @GetMapping("/all")
+    public ResponseEntity getAllJobSeekers(){
+        List<JobSeeker> allJobSeekers = jobSeekerService.getAllJobSeekers();
+
+        return ResponseEntity.ok(allJobSeekers);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getJobSeekerById(@PathVariable("id") Long id){
+        JobSeeker byId = jobSeekerService.getById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(byId);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity createNewJobSeeker(@RequestBody JobSeekerDTO jobSeekerDTO){
+        JobSeeker jobSeeker = jobSeekerService.create(jobSeekerDTO);
+
+        if(jobSeeker==null){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Job seeker could not be created successfully");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(jobSeeker);
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteCourse(@RequestParam(name = "id") Long id) {
+        jobSeekerService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body("Related Job seeker deleted successfully");
+    }
+
+
+
+}
